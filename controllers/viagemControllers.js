c19d29492b4859123a0873c48c7a02ebc761982a
demetriotorgan <demetriotorgan@hotmail.com>
2025-11-01 08:16:13 -0300

    rotaPutEditarViagem

--- controllers/viagemControllers.js
+++ controllers/viagemControllers.js
@@ -63,3 +63,30 @@ module.exports.listarViagens = async(req,res)=>{
         res.status(500).json({msg:'Erro ao listar viagens'});
     }
 };  
+
+module.exports.editarViagem = async(req,res)=>{
+    try {
+        const {id} = req.params;
+        const dadosAtualizados = req.body;
+
+        const viagemExistente = await Viagem.findById(id);
+        if(!viagemExistente){
+            return res.status(404).json(
+                {   sucess:false,
+                    erro:'Viagem não encontrada'
+                });        
+        }
+        Object.assign(viagemExistente, dadosAtualizados);
+        const viagemAtualizada = await viagemExistente.save();
+        res.status(200).json({
+            sucess:true,
+            mensagem:'Viagem atualizada com sucesso'
+        });
+    } catch (error) {
+    console.error('Erro ao editar viagem:', error);
+     res.status(500).json({
+      erro: 'Erro interno ao tentar editar a viagem.',
+      detalhes: error.message
+    });
+    }
+};
--- routes/viagemRoutes.js
+++ routes/viagemRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { adicionarViagem, listarViagens } = require('../controllers/viagemControllers');
+const { adicionarViagem, listarViagens, editarViagem } = require('../controllers/viagemControllers');
 const router = express.Router();
 
 router.post('/salvar-viagem', adicionarViagem);
 router.get('/listar-viagens', listarViagens);
+router.put('/editar-viagem/:id', editarViagem);
 
 module.exports = router;
