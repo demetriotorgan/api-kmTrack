16f228ecf2058084555fc3890da3656e5266e7e7
demetriotorgan <demetriotorgan@hotmail.com>
2025-11-14 10:19:36 -0300

    correcao01

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const cors = require('cors');
+const mongoose = require('mongoose');
 require('dotenv').config();
-const connectDB = require('./config/db');
+const cors = require('cors');
 
-// Rotas
+
+//Rotas
 const viagemRoutes = require('./routes/viagemRoutes');
 const trechoRoutes = require('./routes/trechoRoutes');
 const paradasRoutes = require('./routes/paradasRoutes');
@@ -12,23 +13,25 @@ const pedagios = require('./routes/pedagiosRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
 app.use(express.json());
-app.use(cors({ origin: '*' }));
-
+app.use(cors({
+    origin:'*',
+}));
 app.use((req,res, next)=>{
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
-  next();
-});
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+    next();    
+  });
 
-// 👉 GARANTE QUE O BANCO CONECTA ANTES DAS ROTAS
-connectDB();
 
-app.use('/', viagemRoutes, trechoRoutes, paradasRoutes, abastecimentoRoutes, pedagios);
+mongoose
+    .connect(process.env.DATABASE_URL)
+    .then(()=>console.log('Conectado ao banco ao MongoDB'))
+    .catch((err)=>console.log(err));
 
+app.use('/', viagemRoutes, trechoRoutes, paradasRoutes, abastecimentoRoutes,pedagios);
 app.get('/', (req, res)=>{
-  res.status(200).send('🚀 API de Viagens está online e funcional!');
+    res.status(200).send('🚀 API de Viagens está online e funcional!');
 });
 
 app.listen(PORT, ()=>console.log(`Rodando na porta ${PORT}`));
